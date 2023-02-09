22a1a52eca9d43954e5169e84d95b45fbb530ad8
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-02-09 22:58:34 +0100

    Beginning of shell ui

---
+++ console.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import
+class HBNBCommand(cmd.Cmd):
+	"""Initialize the command line"""
+	intro = 'ALX Software Engineering Command line'
+	prompt= '(hbnb)'
+if __name__ = "__main__":
+	HBNBCommand().cmdloop()
