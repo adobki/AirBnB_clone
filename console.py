5c55a851a5b25b94eeb08b8791c3a16011e09fad
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-02-09 23:27:03 +0100

    arg, fix

--- console.py
+++ console.py
@@ -2,20 +2,26 @@
 import cmd, sys
 class HBNBCommand(cmd.Cmd):
 	"""Initialize the command line"""
-	intro = 'ALX Software Engineering Command line'
+	intro = 'ALX Software Engineering Command line. Type help or ? to list commands.'
 	prompt= '(hbnb)'
+	file = None
 	print('EOF help quit')
 
 	# -- Quit the shell
-	def do_quit(self):
-		'Quit command to exit the program'
+	def do_quit(self, arg):
+		'Quit command to exit the program(quit)'
 		self.close()
 		quit()
+
+	# -- EOF (End Of File)
+	def do_EOF(self, arg):
+		'End Of File (EOF)'
+		EOF()
 
 	def close(self):
 		if self.file:
 			self.file.close()
 			self.file = None
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	HBNBCommand().cmdloop()
