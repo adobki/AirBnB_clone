de3486df97ce8e16f2c3914c633ee10eec38198c
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-02-09 23:10:55 +0100

    minor edits

--- console.py
+++ console.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-import
+import cmd, sys
 class HBNBCommand(cmd.Cmd):
 	"""Initialize the command line"""
 	intro = 'ALX Software Engineering Command line'
 	prompt= '(hbnb)'
+
+	# -- Quit the shell
+	def do_quit(self):
+		'Quit command to exit the program'
+		self.close()
+		quit()
+
+	def close(self):
+		if self.file:
+			self.file.close()
+			self.file = None
+
 if __name__ = "__main__":
 	HBNBCommand().cmdloop()
