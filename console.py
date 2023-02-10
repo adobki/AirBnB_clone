dbfe4ae41746ba71cd14167a0d7950d64633f7fc
Donald Ajaps <ajapon@yahoo.com>
2023-02-10 10:09:00 +0100

    Console 0.0.1: Fix console.py
    Code in console.py was updated to meet the given requirements for the task. New lines were added to complete the Class, unnecessary lines were removed, and adjustments were made across entire code for compliance with PyCodeStyle (PEP 8).

--- console.py
+++ console.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-import cmd, sys
+"""Command interpreter to issue commands for object management in AirBnb."""
+import cmd
+
+
 class HBNBCommand(cmd.Cmd):
-	"""Initialize the command line"""
-	intro = 'ALX Software Engineering Command line. Type help or ? to list commands.'
-	prompt= '(hbnb)'
-	file = None
-	print('EOF help quit')
+    """Entry point of the command interpreter."""
 
-	# -- Quit the shell
-	def do_quit(self, arg):
-		'Quit command to exit the program(quit)'
-		self.close()
-		quit()
+    # Initialize the command line
+    intro = 'ALX Software Engineering Command line. ' \
+            'Type help or ? to list commands.'
+    prompt = '(hbnb) '
+    file = None
 
-	# -- EOF (End Of File)
-	def do_EOF(self, arg):
-		'End Of File (EOF)'
-		EOF()
+    # -- Quit the shell
+    def do_quit(self):
+        """Quit command to exit the program (quit)"""
+        self.close()
+        quit()
 
-	def close(self):
-		if self.file:
-			self.file.close()
-			self.file = None
+    # -- EOF (End Of File)
+    def do_EOF(self):
+        """End Of File (EOF)"""
+        self.do_quit()
+
+    def close(self):
+        """Closes the file in self.file variable if it is open."""
+        if self.file:
+            self.file.close()
+            self.file = None
+
+    def emptyline(self):
+        """Prevents empty line/blank input from running previous command."""
+        pass
+
 
 if __name__ == "__main__":
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
